d6fe9fce24ea1ce1ad68721637a135a0a4ec77df
Proxtron <51761303+Proxtron@users.noreply.github.com>
2025-06-12 20:52:39 -0700

    Setup webpack configuration and initial file structure

--- webpack.common.js
+++ webpack.common.js
@@ -1,22 +1,19 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: "development",
-    entry: "./src/index.js",
-    output: {
-        filename: "main.js",
-        path: path.resolve(__dirname, "dist"),
-        clean: true
+    entry: {
+        app: './src/index.js',
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: "./src/template.html",
+            template: "./src/template.html"
         })
     ],
-    devtool: "eval-source-map",
-    devServer: {
-    watchFiles: ["./src/template.html"],
+    output: {
+        filename: 'index.bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
     },
     module: {
         rules: [
